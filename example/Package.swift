2d8ed0b98eb2ee20ef22f24180d97e804a75cec6
Benjamin Herzog <herzog.benjamin@me.com>
2015-12-13 20:38:37 +0100

    Added example

---
+++ example/Package.swift
@@ -0,0 +1,9 @@
+import PackageDescription
+
+let package = Package(
+    name: "Hello",
+    dependencies: [
+//        .Package(url: "https://github.com/kylef/Curassow.git", majorVersion: 0, minor: 1),
+//        .Package(url: "https://github.com/jensravens/interstellar.git", majorVersion: 1),
+    ]
+)
---
+++ example/Sources/main.swift
@@ -0,0 +1,9 @@
+// import Curassow
+// import Inquiline
+
+
+// serve { request in
+//   return Response(.Ok, contentType: "text/plain", body: "Hello World")
+// }
+
+print("Hello World")
