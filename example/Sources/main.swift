7278d98a77ff50dec0dd78e81cfe91e542698783
Benjamin Herzog <herzog.benjamin@me.com>
2015-12-13 20:14:29 +0000

    Tried to build on ubuntu

--- example/Sources/main.swift
+++
@@ -1,9 +0,0 @@
-// import Curassow
-// import Inquiline
-
-
-// serve { request in
-//   return Response(.Ok, contentType: "text/plain", body: "Hello World")
-// }
-
-print("Hello World")
---
+++ example/main.swift
@@ -0,0 +1,8 @@
+import Curassow
+import Inquiline
+
+
+serve { request in
+  return Response(.Ok, contentType: "text/plain", body: "Hello World")
+}
+
