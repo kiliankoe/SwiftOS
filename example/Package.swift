2a0199afed0e3c8ef8fe8703575831ad546b52d6
Kilian Koeltzsch <me@kilian.io>
2015-12-14 16:26:18 +0100

    working minimal curassow example
    explicitly importing Glibc helped fix some compiling issues

--- example/Package.swift
+++ example/Package.swift
@@ -1,5 +1,8 @@
 import PackageDescription
 
 let package = Package(
-	name: "Example"
+	name: "Example",
+	dependencies: [
+		.Package(url: "git@github.com:kylef/Curassow.git", majorVersion: 0, minor: 1),
+	]
 )
--- example/Sources/main.swift
+++ example/Sources/main.swift
@@ -1,2 +1,14 @@
-let v = Vertex(x: 10.0, y: 20.0)
-print(v.x)
+import Curassow
+import Inquiline
+import Glibc // https://bugs.swift.org/browse/SR-92
+
+serve { request in
+        switch request.path {
+        case "/":
+                return Response(.Ok, contentType: "text/plain", body: "root")
+        case "/hello":
+                return Response(.Ok, contentType: "text/plain", body: "Hello World!")
+        default:
+                return Response(.Ok, contentType: "text/plain", body: "none of the above")
+        }
+}
