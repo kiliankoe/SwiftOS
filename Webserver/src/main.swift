47189d4586d7e29afe2c224a4dd94f48043f6dda
Benjamin Herzog <herzog.benjamin@me.com>
2015-12-16 09:05:27 +0000

    Use correct optional handling (prevents crash by jenkins.. :D)

--- Webserver/src/main.swift
+++ Webserver/src/main.swift
@@ -8,12 +8,14 @@ import Epoch
 import CHTTPParser
 import CLibvenice
 
+let textSize = 200
+
 struct ServerResponder: ResponderType {
     func respond(request: Request) -> Response {
-	let path = request.uri.path!
-	let text = path.splitBy("/").last!
+	let path = request.uri.path
+	let text = path?.splitBy("/").last ?? ""
         // do something based on the Request
-        return Response(status: .OK, body: "<html><head><meta charset=\"utf-8\"></head><h1>\(text)</h1></html>")
+        return Response(status: .OK, body: "<html><head><meta charset=\"utf-8\"></head><center><p style=\"font-size:\(textSize)px\">\(text)</p></center></html>")
     }
 }
 
